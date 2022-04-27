7ea6b102f06d24fc6fab388d6a9c8ec70a934da8
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-04-27 19:09:53 +0300

    Заинжектил фабрику в контроллер

---
+++ src/main/java/ru/zolotenkov/patterns/abstractfactory/FactoryController.java
@@ -0,0 +1,28 @@
+package ru.zolotenkov.patterns.abstractfactory;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.zolotenkov.patterns.abstractfactory.factory.Factory;
+import ru.zolotenkov.patterns.abstractfactory.model.Armchair;
+import ru.zolotenkov.patterns.abstractfactory.model.Chair;
+import ru.zolotenkov.patterns.abstractfactory.model.Table;
+
+@RestController
+public class FactoryController {
+  private final Factory factory;
+
+  public FactoryController(Factory factory) {
+    this.factory = factory;
+  }
+
+  @GetMapping("/factory/create")
+  public void createFurniture() {
+    Armchair armchair = factory.createArmchair();
+    Chair chair = factory.createChair();
+    Table table = factory.createTable();
+    armchair.info();
+    chair.info();
+    table.info();
+  }
+}
