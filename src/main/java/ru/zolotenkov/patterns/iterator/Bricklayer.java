739bb42042adef8a44bf1063d7261917720c102c
sergei <ulazolote@g.mail.cjm>
2022-05-02 18:49:51 +0300

    refactor

--- src/main/java/ru/zolotenkov/patterns/iterator/Bricklayer.java
+++ src/main/java/ru/zolotenkov/patterns/iterator/Bricklayer.java
@@ -7,6 +7,6 @@ public class Bricklayer {
     private final Logger logger = LoggerFactory.getLogger(Bricklayer.class);
 
     void lay(Brick brick) {
-        logger.info("Laying brick {}", brick);
+        logger.info("Laying {}", brick.getClass().getSimpleName());
     }
 }
