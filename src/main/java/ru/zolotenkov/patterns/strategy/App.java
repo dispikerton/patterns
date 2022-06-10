92ff0a0879c1fb464faadc5427083a7fabd99fcb
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-06-10 11:54:55 +0300

    рефакторинг

--- src/main/java/ru/zolotenkov/patterns/strategy/App.java
+++ src/main/java/ru/zolotenkov/patterns/strategy/App.java
@@ -5,8 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class App {
   public static void main(String[] args) {
-    Strategy mongo = new MongoDB();
-    Context context = new Context(mongo);
+    Context context = new Context(new MongoDB());
     log.info("data 1 {}", context.getData());
     context.setStrategy(new PostgreSQL());
     log.info("data 2 {}", context.getData());
