78f4bbc0b53be639f1cc9d7cc2c96ddd06558727
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-06-24 15:43:31 +0300

    delete @Slf4j

--- src/main/java/ru/zolotenkov/patterns/templatemethod/Developer.java
+++ src/main/java/ru/zolotenkov/patterns/templatemethod/Developer.java
@@ -1,11 +1,8 @@
 package ru.zolotenkov.patterns.templatemethod;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Developer implements Employee {
   @Override
   public void work() {
-    log.info("Пишу код");
+    logger.info("Пишу код");
   }
 }
--- src/main/java/ru/zolotenkov/patterns/templatemethod/ProjectManager.java
+++ src/main/java/ru/zolotenkov/patterns/templatemethod/ProjectManager.java
@@ -1,11 +1,8 @@
 package ru.zolotenkov.patterns.templatemethod;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class ProjectManager implements Employee {
   @Override
   public void work() {
-    log.info("Иду на созвон");
+    logger.info("Иду на созвон");
   }
 }
